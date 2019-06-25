88eae8ba2190cb5702a215c7c69419f169cb6b43
loveleshsaxena <er.saxena27@gmail.com>
2019-06-26 00:01:48 +0530

    Add AlphaClient: Config file for client, have details about AlphaServer like IP and Port

---
+++ AlphaClient/AlphaClient.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Code for AlphaClient SetUp
+bashRCContent=$(<~/.bashrc)
+
+lookUpLine="Code for AlphaClient"
+
+if [[ $bashRCContent =~ $lookUpLine ]]; then
+    echo "AlphaClient already configured on this machine"
+else
+    echo "AlphaClient not configured on this machine. Will start configuring..."
+    cat AlphaClient.config >> ~/.bashrc
+fi
